2e8bec126e2f9a8dbebbe9e1ea878ced1bd5c903
vzdee <mario.mendoza716@gmail.com>
2026-09-05 19:16:31 -0600

    feat: Implemented automatic navbar closing

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -2,9 +2,37 @@
 const btnMenuToggle = document.getElementById('menu-toggle');
 // get the menu mobile
 const mobileMenu = document.getElementById('menu-mobile');
+// get all a elements in the menu mobile
+const mobileMenuElements = mobileMenu.querySelectorAll('a, button');
 
 // toggle menu mobile
 btnMenuToggle.addEventListener('click', () => {
+    // toggle the states for the btn and menu
     btnMenuToggle.classList.toggle('is-open');
     mobileMenu.classList.toggle('is-open');
 });
+
+// close menu mobile when a link is clicked
+mobileMenuElements.forEach((element) => {
+    // add event listener to each link element
+    element.addEventListener('click', closeMenuMobile);
+});
+
+// listener when the click is outside the menu
+document.addEventListener('click', (event) => {
+    // check if the menu is open
+    if (mobileMenu.classList.contains('is-open')){
+        // console.log('menu is open');
+        if (!btnMenuToggle.contains(event.target) && !mobileMenu.contains(event.target)){
+            closeMenuMobile();
+            // depuring
+            // console.log('click outside menu');
+        }
+    }
+});
+
+// function to close menu mobile
+function closeMenuMobile() {
+    btnMenuToggle.classList.remove('is-open');
+    mobileMenu.classList.remove('is-open');
+}
